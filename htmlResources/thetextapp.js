218062daceaf60244f3611a310927b3802174bec
Eliran Wong <hkwebtech@gmail.com>
2019-01-17 20:43:45 +0000

    added support of mutual highlighting

--- htmlResources/thetextapp.js
+++ htmlResources/thetextapp.js
@@ -22,3 +22,37 @@ function cr(b,c,v) {
     document.title = "BIBLE:::"+mainText+":::"+verseReference;
 
 }
+
+function hl1(id, cl, sn) {
+
+if (cl != '') {
+	w3.addStyle('.c'+cl,'background-color','PAPAYAWHIP');
+}
+if (sn != '') {
+	w3.addStyle('.G'+sn,'background-color','#E7EDFF');
+}
+if (id != '') {
+	var focalElement = document.getElementById('w'+id);
+	if (focalElement != null) {
+		document.getElementById('w'+id).style.background='#C9CFFF';
+	}
+}
+
+}
+
+function hl0(id, cl, sn) {
+
+if (cl != '') {
+	w3.addStyle('.c'+cl,'background-color','');
+}
+if (sn != '') {
+	w3.addStyle('.G'+sn,'background-color','');
+}
+if (id != '') {
+	var focalElement = document.getElementById('w'+id);
+	if (focalElement != null) {
+		document.getElementById('w'+id).style.background='';
+	}
+}
+
+}
