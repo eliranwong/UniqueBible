928ffeef1bbdd4abd35e2da51a5842285ab0894e
Eliran Wong <hkwebtech@gmail.com>
2019-02-01 12:01:51 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,37 @@
+myGoogleApiKey = ''
+instantInformationEnabled = 1
+fontSize = 130
+mainText = 'BSB'
+mainB = 2
+mainC = 1
+mainV = 1
+studyText = 'NET'
+studyB = 12
+studyC = 2
+studyV = 14
+commentaryText = 'CBSC'
+commentaryB = 12
+commentaryC = 2
+commentaryV = 14
+topic = 'EXLBT'
+dictionary = 'EAS'
+encyclopedia = 'ISB'
+book = 'Simpson_But_God'
+bookSearchString = ''
+historyRecordAllowed = 50
+currentRecord = {'main': 0, 'study': 0}
+history = {'main': ['Genesis 1:1',
+          'BIBLE:::KJV:::Gen 1:1',
+          'BIBLE:::KJV:::Gen 22:1',
+          'BIBLE:::KJV:::Gen 24:1',
+          'text:::NIV',
+          'Exod 1',
+          'text:::EXP',
+          'text:::BSB'],
+ 'study': ['BIBLE:::NET:::John 3:16',
+           'BOOK:::Boyce:::02 - Chapter1',
+           'BOOK:::Hartill:::0 - Title',
+           'BOOK:::Simpson_But_God:::01 - Preface',
+           'BOOK:::Simpson_But_God:::02 - Contents',
+           'BOOK:::Simpson_But_God:::03 - Chapter1',
+           'BIBLE:::NET:::2Kgs 2:14']}
