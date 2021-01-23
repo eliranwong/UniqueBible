b000350bd24a8b50698fbaa5f075f87794da61d2
Oliver Tseng <otseng@yahoo.com>
2021-01-22 21:32:50 -0500

    Add next/previous verse/chapter buttons to Aleph menu

---
+++ tests/BibleBooks_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from BibleBooks import BibleBooks
+
+class BibleBooksTestCase(unittest.TestCase):
+
+    # @unittest.skip
+    def test_getLastChapter(self):
+        self.assertEqual(BibleBooks.getLastChapter(1), 50)
+        self.assertEqual(BibleBooks.getLastChapter(2), 40)
+        self.assertEqual(BibleBooks.getLastChapter(66), 22)
+        self.assertEqual(BibleBooks.getLastChapter(100), 100)
+
+
