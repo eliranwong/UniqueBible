d75d9c638c754d321555271de810d2e188888156
Eliran Wong <hkwebtech@gmail.com>
2021-01-26 12:42:13 +0000

    Update shortcut_uba_chromeOS_fcitx.sh

--- shortcut_uba_chromeOS_fcitx.sh
+++ shortcut_uba_chromeOS_fcitx.sh
@@ -2,5 +2,5 @@
 export QT_QPA_PLATFORM=wayland
 export QT_IM_MODULE=fcitx
 /usr/bin/urxvt &
-source /home/eliranwong/UniqueBible/venv/bin/activate
-/home/eliranwong/UniqueBible/venv/bin/python3 /home/eliranwong/UniqueBible/main.py
+source $HOME/UniqueBible/venv/bin/activate
+$HOME/UniqueBible/venv/bin/python3 /home/eliranwong/UniqueBible/main.py
