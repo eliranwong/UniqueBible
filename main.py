59d3fa089afe862db8fbf277f4879009ab77c181
Tseng, Oliver <otseng@yahoo.com>
2021-03-05 23:10:40 -0500

    Fix CLI

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import os, platform, logging, re, sys
 import logging.handlers as handlers
+import socket
 
 thisFile = os.path.realpath(__file__)
 wd = thisFile[:-7]
@@ -40,8 +41,20 @@ if config.enableLogging:
 else:
     logger.addHandler(logging.NullHandler())
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 # Remote CLI
-if initialCommand == "cli":
+if (len(sys.argv) > 1) and sys.argv[1] == "cli":
     try:
         import telnetlib3
     except:
