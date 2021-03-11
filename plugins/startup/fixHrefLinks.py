9f02609c31dbe1d5ee7221884ea9b5c00ce26c8f
Eliran Wong <hkwebtech@gmail.com>
2021-03-11 18:49:51 +0000

    uploaded a startup plugin

---
+++ plugins/startup/fixHrefLinks.py
@@ -0,0 +1,7 @@
+import config, re
+
+def fixHrefLinks(text):
+    return re.sub(r"""href=(['"])(http.*?)\1""", r"href='#' onclick={0}document.title='_command:::online:::\2'; document.title='online:::\2'{0}".format('"'), text)
+
+config.bibleWindowContentTransformers.append(fixHrefLinks)
+config.studyWindowContentTransformers.append(fixHrefLinks)
