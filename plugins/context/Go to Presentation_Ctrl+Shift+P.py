3c361193b9d77a551ef3ef49367eaee47add172d
eliranwong <hkwebtech@gmail.com>
2021-03-17 11:04:27 +0000

    ver 22.85; Added a gui menu plugin "Configure Presentation"; hide empty verses for comparison

--- plugins/context/Go to Presentation_Ctrl+Shift+P.py
+++ plugins/context/Go to Presentation_Ctrl+Shift+P.py
@@ -1,8 +1,12 @@
 import config
 
-config.shortcutGotoPresentation = "G"
+presentationParser = config.presentationParser
+
+config.presentationParser = True
 
 if config.pluginContext:
     config.mainWindow.runTextCommand("SCREEN:::{0}".format(config.pluginContext))
 else:
     config.contextSource.messageNoSelection()
+
+config.presentationParser = presentationParser
