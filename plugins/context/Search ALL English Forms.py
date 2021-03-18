44bcbf2c38a090b11b985f0b3478827c9046673c
Eliran Wong <hkwebtech@gmail.com>
2021-03-18 23:27:00 +0000

    added 2 context plugins

---
+++ plugins/context/Search ALL English Forms.py
@@ -0,0 +1,27 @@
+import config, sys
+from install.module import *
+try:
+    # https://pypi.org/project/word-forms/
+    from word_forms.word_forms import get_word_forms
+    moduleInstalled = True
+except:
+    moduleInstalled = False
+
+# Install essential module if it is absent
+if not moduleInstalled:
+    installmodule("-U word_forms")
+if not "pypinyin" in sys.modules:
+    try:
+        from word_forms.word_forms import get_word_forms
+    except:
+        config.mainWindow.displayMessage("This plugin is not enabled.\nRun 'pip3 install -U {0}' to install essential module first.".format("word_forms"))
+
+if config.pluginContext:
+    allForms = get_word_forms(config.pluginContext)
+    formList = []
+    for form in allForms.values():
+        if form:
+            formList += list(form)
+    config.mainWindow.runTextCommand("ORSEARCH:::{0}".format("|".join(set(formList))))
+else:
+    config.contextSource.messageNoSelection()
---
+++ plugins/context/Webster.py
@@ -0,0 +1,26 @@
+import config, sys
+from install.module import *
+try:
+    # https://pypi.org/project/lemmagen3/
+    from lemmagen3 import Lemmatizer
+    moduleInstalled = True
+except:
+    moduleInstalled = False
+
+# Install essential module if it is absent
+if not moduleInstalled:
+    installmodule("lemmagen3")
+if not "pypinyin" in sys.modules:
+    try:
+        from lemmagen3 import Lemmatizer
+    except:
+        config.mainWindow.displayMessage("This plugin is not enabled.\nRun 'pip3 install {0}' to install essential module first.".format("lemmagen3"))
+
+if config.pluginContext:
+    lemma = Lemmatizer('en').lemmatize(config.pluginContext)
+    config.mainWindow.runTextCommand("SPEAK:::en-gb:::{0}".format(lemma))
+    # Search multiple thrid-party dicitonaries
+    for thridDict in ("webster",):
+        config.mainWindow.runTextCommand("SEARCHTHIRDDICTIONARY:::{0}:::{1}".format(thridDict, lemma))
+else:
+    config.contextSource.messageNoSelection()
