db5da75d32e701587a06d13f92e1b6459e18e51c
eliranwong <hkwebtech@gmail.com>
2021-03-22 15:07:10 +0000

    posting text to gmail with line breaks

--- plugins/context/Gmail.py
+++ plugins/context/Gmail.py
@@ -1,6 +1,7 @@
 import config, webbrowser
 
 if config.pluginContext:
-    webbrowser.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&su=UniqueBible.app&body={0}".format(config.pluginContext))
+    content = config.pluginContext.replace("\n", "%0D%0A")
+    webbrowser.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&su=UniqueBible.app&body={0}".format(content))
 else:
     webbrowser.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm")
