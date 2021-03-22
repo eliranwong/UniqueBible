31128abc21a12e5f9a65df1fbaa6e6c1aa26509e
Eliran Wong <hkwebtech@gmail.com>
2021-03-22 14:47:53 +0000

    Add files via upload

---
+++ plugins/context/Gmail.py
@@ -0,0 +1,6 @@
+import config, webbrowser
+
+if config.pluginContext:
+    webbrowser.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&su=UniqueBible.app&body={0}".format(config.pluginContext))
+else:
+    config.contextSource.messageNoSelection()
