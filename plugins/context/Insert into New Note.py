ef6925f91a029d8d112c828b5dd777fd0c6c6ee0
Eliran Wong <hkwebtech@gmail.com>
2021-03-23 00:24:07 +0000

    Delete Insert into New Note.py

--- plugins/context/Insert into New Note.py
+++
@@ -1,8 +0,0 @@
-import config
-
-if config.pluginContext:
-    config.contextItem = config.pluginContext
-    config.mainWindow.createNewNoteFile()
-else:
-    config.contextSource.messageNoSelection()
- 
