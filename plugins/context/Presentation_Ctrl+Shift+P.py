9128c4580511a4a2702134ee91a3903ac73182f1
Eliran Wong <hkwebtech@gmail.com>
2021-03-27 21:01:02 +0000

    Add files via upload

---
+++ plugins/context/Presentation_Ctrl+Shift+P.py
@@ -0,0 +1,12 @@
+import config
+
+presentationParser = config.presentationParser
+
+config.presentationParser = True
+
+if config.pluginContext:
+    config.mainWindow.runTextCommand("SCREEN:::{0}".format(config.pluginContext))
+else:
+    config.contextSource.messageNoSelection()
+
+config.presentationParser = presentationParser
