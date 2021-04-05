665c19bfad55f8d6262b4f7e4dbaf8a7c637399e
Eliran Wong <hkwebtech@gmail.com>
2021-04-06 00:31:26 +0100

    Delete Clear Study Tabs.py

--- plugins/context/Clear Study Tabs.py
+++
@@ -1,6 +0,0 @@
-import config
-
-for tab in range(0, config.numberOfTab):
-    config.mainWindow.studyView.setCurrentIndex(tab)
-    config.mainWindow.studyView.setHtml("", "")
-    config.mainWindow.studyView.setTabText(tab, "Study")
