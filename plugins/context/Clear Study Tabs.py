84c8a6c27dcbd8749c69b5604ab1730b1f2f47ef
Tseng, Oliver <otseng@yahoo.com>
2021-04-05 17:53:38 -0400

    Add ability to clear tabs

---
+++ plugins/context/Clear Study Tabs.py
@@ -0,0 +1,6 @@
+import config
+
+for tab in range(0, config.numberOfTab):
+    config.mainWindow.studyView.setCurrentIndex(tab)
+    config.mainWindow.studyView.setHtml("", "")
+    config.mainWindow.studyView.setTabText(tab, "Study")
