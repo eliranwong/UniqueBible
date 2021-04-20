a393f2a3b133625d87acb087a7a49ac956dbe072
Eliran Wong <hkwebtech@gmail.com>
2021-04-20 23:29:13 +0100

    ver 23.76; prevent plugin "ePub Viewer" from loading on Windows, as it causes crashes on Windows

--- plugins/menu/ePub Viewer.py
+++ plugins/menu/ePub Viewer.py
@@ -1,10 +1,11 @@
-import config
-import os
+import config, os, platform
 from qtpy.QtCore import QUrl
 
-
-config.mainWindow.studyView.setTabText(config.mainWindow.studyView.currentIndex(), "EPUB")
-config.mainWindow.studyView.setTabToolTip(config.mainWindow.studyView.currentIndex(), "EPUB")
-file = os.path.join(os.getcwd(), './plugins/menu/ePubViewer/ePubViewer.html')
-qurl = QUrl.fromLocalFile(file)
-config.mainWindow.studyView.load(qurl)
+if platform.system() == "Windows":
+    config.mainWindow.displayMessage("This plugin does not work on Windows.")
+else:
+    config.mainWindow.studyView.setTabText(config.mainWindow.studyView.currentIndex(), "EPUB")
+    config.mainWindow.studyView.setTabToolTip(config.mainWindow.studyView.currentIndex(), "EPUB")
+    file = os.path.join(os.getcwd(), './plugins/menu/ePubViewer/ePubViewer.html')
+    qurl = QUrl.fromLocalFile(file)
+    config.mainWindow.studyView.load(qurl)
