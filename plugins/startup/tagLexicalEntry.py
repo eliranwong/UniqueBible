02d8fb9925ae317f8689268d99cd15d327066a91
Tseng, Oliver <otseng@yahoo.com>
2021-04-23 04:50:38 -0400

    Fix second Strongs

--- plugins/startup/tagLexicalEntry.py
+++ plugins/startup/tagLexicalEntry.py
@@ -10,10 +10,10 @@ import config, re
 
 def tagLexicalEntry(text):
     searchReplace = (
-        (" [ ]+?([^ ])", r" \1"),
-        ("""(<ref onclick=["']lex\(["'])([^\(\)]+?)(["']\)["'])>""", r"""\1\2\3 onmouseover="ld('\2')">"""),
-        (" ([EHG][0-9]+?) ", r""" <sup><ref onclick="lex('\1')" onmouseover="ld('\1')">\1</ref></sup> """),
-        #("([EHG][0-9]+?) ", r"""<ref onclick="lex('\1')" onmouseover="ld('\1')">\1</ref> """),
+        (r" [ ]+?([^ ])", r" \1"),
+        (r"""(<ref onclick=["']lex\(["'])([^\(\)]+?)(["']\)["'])>""", r"""\1\2\3 onmouseover="ld('\2')">"""),
+        (r" ([EHG][0-9]+?) ", r""" <sup><ref onclick="lex('\1')" onmouseover="ld('\1')">\1</ref></sup> """),
+        (r" ([EHG][0-9]+?) ", r""" <sup><ref onclick="lex('\1')" onmouseover="ld('\1')">\1</ref></sup> """),
     )
     for search, replace in searchReplace:
         text = re.sub(search, replace, text)
