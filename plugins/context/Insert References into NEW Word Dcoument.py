01320d0660259d19c75890089fccd1ddfcd5c61c
Eliran Wong <hkwebtech@gmail.com>
2021-05-03 18:51:13 +0100

    ver 24.06; added 5 and renamed 1 context plugins.

---
+++ plugins/context/Insert References into NEW Word Dcoument.py
@@ -0,0 +1,24 @@
+import config, subprocess
+from BibleVerseParser import BibleVerseParser
+
+if config.isPythonDocxInstalled:
+    from docx import Document
+
+    if config.pluginContext:
+        parser = BibleVerseParser(config.parserStandarisation)
+        verseList = parser.extractAllReferences(config.pluginContext, False)
+        if not verseList:
+            config.mainWindow.displayMessage(config.thisTranslation["message_noReference"])
+        else:
+            references = "; ".join([parser.bcvToVerseReference(*verse) for verse in verseList])
+
+            document = Document()
+            document.add_paragraph(references)
+            filename = "WordDocument_NEW.docx"
+            document.save(filename)
+            subprocess.Popen("{0} {1}".format(config.open, filename), shell=True)
+    else:
+        config.contextSource.messageNoSelection()
+
+else:
+    config.mainWindow.displayMessage(config.thisTranslation["message_noSupport"])
