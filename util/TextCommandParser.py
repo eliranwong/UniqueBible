cad3723c950317640dfee76564b8eb297fa4c586
Eliran Wong <hkwebtech@gmail.com>
2021-06-02 10:41:41 +0100

    fixed a tag

--- util/TextCommandParser.py
+++ util/TextCommandParser.py
@@ -923,7 +923,7 @@ class TextCommandParser:
                 bcvTuple = verseList[-1]
             content = self.hideLexicalEntryInBible(content)
             # Add text tag for custom font styling
-            content = "<text class='{0}'>{1}</text>".format(text, content)
+            content = "<bibletext class='{0}'>{1}</bibletext>".format(text, content)
             if config.openBibleInMainViewOnly:
                 self.setMainVerse(text, bcvTuple)
                 self.setStudyVerse(text, bcvTuple)
