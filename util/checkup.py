e9941a907125480adb17466a4f143db3d9a0ad43
Oliver Tseng <otseng@yahoo.com>
2022-08-19 21:34:08 -0400

    Update checkup.py

--- util/checkup.py
+++ util/checkup.py
@@ -372,6 +372,8 @@ def isNltkInstalled():
         return False
 
 def isWordformsInstalled():
+    if config.enableBinaryExecutionMode:
+        return False
     import ssl
     try:
         _create_unverified_https_context = ssl._create_unverified_context
@@ -396,6 +398,9 @@ def isPaddleocrInstalled():
         return False
 
 def isApswInstalled():
+    if config.enableBinaryExecutionMode:
+        return False
+
     try:
         import apsw
         return True
@@ -410,6 +415,8 @@ def isPyluachInstalled():
         return False
 
 def isChineseEnglishLookupInstalled():
+    if config.enableBinaryExecutionMode:
+        return False
     try:
         from chinese_english_lookup import Dictionary
         config.cedict = Dictionary()
@@ -418,6 +425,8 @@ def isChineseEnglishLookupInstalled():
         return False
 
 def isLemmagen3Installed():
+    if config.enableBinaryExecutionMode:
+        return False
     # Note: It looks like that lemmagen3 is a better lemmatizer than using "word_forms.lemmatize" installed with word_forms package
     try:
         from lemmagen3 import Lemmatizer
