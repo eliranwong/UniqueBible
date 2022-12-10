9e3c746685c8c6fd34d3c8ca0bb00484393d28ba
Oliver Tseng <otseng@yahoo.com>
2022-12-10 16:42:58 -0500

    Ver 33.28; Fix accessing Chinese commentaries in api-server (#260)

--- util/RemoteApiHandler.py
+++ util/RemoteApiHandler.py
@@ -5,6 +5,7 @@ import json
 import logging
 import os
 import re
+import urllib
 from datetime import datetime, timezone
 from pathlib import Path
 
@@ -44,7 +45,6 @@ class RemoteApiHandler(ApiRequestHandler):
         self.logger = logging.getLogger('uba')
         config.internet = True
         config.showHebrewGreekWordAudioLinks = False
-        CatalogUtil.loadLocalCatalog()
         try:
             super().__init__(*args, directory="htmlResources", **kwargs)
         except Exception as ex:
@@ -100,8 +100,13 @@ class RemoteApiHandler(ApiRequestHandler):
         if "?" in request:
             request = request.split("?")[0]
         self.jsonData['request'] = request
+        config.marvelData = 'marvelData'
         if query:
             self.jsonData['query'] = query
+            if "lang" in query.keys():
+                lang = query["lang"][0]
+                if os.path.exists('marvelData_' + lang):
+                    config.marvelData = 'marvelData_' + lang
         cmd = request[1:].split("/")
         if len(cmd) > 0:
             command = cmd[0].lower()
@@ -143,7 +148,7 @@ class RemoteApiHandler(ApiRequestHandler):
         if cmd[1].lower() == "bible":
             if cmd[2].lower() == "abbreviations":
                 lang = "eng"
-                if query:
+                if query and "lang" in query.keys():
                     lang = query["lang"][0]
                 data = []
                 for key, value in BibleBooks().abbrev[lang].items():
@@ -210,6 +215,7 @@ class RemoteApiHandler(ApiRequestHandler):
     # /book/Hymn+Lyrics+-+English
     # /book/Hymn+Lyrics+-+English/Amazing+Grace
     def processBookCommand(self, cmd):
+        CatalogUtil.reloadLocalCatalog()
         if len(cmd) == 1:
             self.jsonData['data'] = [book for book in CatalogUtil.getBooks()]
             return
@@ -217,29 +223,34 @@ class RemoteApiHandler(ApiRequestHandler):
             self.sendError("Invalid Book command")
             return
         module = cmd[1].replace("+", " ")
+        module = urllib.parse.unquote(module)
         if len(cmd) == 2:
             self.jsonData['data'] = [topic for topic in Book(module).getTopicList()]
         else:
             chapter = cmd[2].replace("+", " ")
-            chapter = chapter.replace("%3C", "<")
-            chapter = chapter.replace("%3E", ">")
+            chapter = urllib.parse.unquote(chapter)
+            # chapter = chapter.replace("%3C", "<")
+            # chapter = chapter.replace("%3E", ">")
             data = Book(module).getContentByChapter(chapter)
             self.jsonData['data'] = data if data else ("[Not found]",)
 
     # /commentary/ABC/43/1
     def processCommentaryCommand(self, cmd):
+        config.commentariesFolder = os.path.join(config.marvelData, "commentaries")
         if len(cmd) == 1:
             self.jsonData['data'] = [commentary for commentary in Commentary().getCommentaryList()]
             return
         elif len(cmd) < 4:
             self.sendError("Invalid Commentary command")
             return
-        data = Commentary(cmd[1]).getRawContent(cmd[2], cmd[3])
+        commentary = urllib.parse.unquote(cmd[1])
+        data = Commentary(commentary).getRawContent(cmd[2], cmd[3])
         self.jsonData['data'] = data if data else ("[Not found]",)
 
     # /lexicon
     # /lexicon/TBESG/G5
     def processLexiconCommand(self, cmd):
+        CatalogUtil.reloadLocalCatalog()
         if len(cmd) == 1:
             self.jsonData['data'] = [lexicon for lexicon in LexiconData().lexiconList]
             return
